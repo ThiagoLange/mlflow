69cd8bd041602a7d11acb604d689cb2e51cdb73a
Thiago Egon Lange <thiago_lange@yahoo.com.br>
2024-03-15 17:21:44 -0300

    Create projects function by utils

--- 01_create_experiment.py
+++ 01_create_experiment.py
@@ -1,11 +1,16 @@
+# import package
 import mlflow
+from mlflow_utils import create_mlflow_experiment
 
 if __name__ == "__main__":
     # create a new mlflow experiment
-    experiment_id = mlflow.create_experiment(
-        name="testing_mlflow1",
-        artifact_location="testing_mlflow1_artifacts",
-        tags={"env": "dev", "version": "1.0.0"},
-    )
+    # experiment_id = mlflow.create_experiment(
+    #     name="testing_mlflow1",
+    #     artifact_location="testing_mlflow1_artifacts",
+    #     tags={"env": "dev", "version": "1.0.0"},
+    # )
 
-    print(experiment_id)
+    # print(experiment_id)
+
+    experiment_id = create_mlflow_experiment(experiment_name="testing_mlflow2", artifact_location="testing_mlflow1_artifacts", tags={"env:": "dev", "version": "1.0.0"})
+    print(f"Experiment ID: {experiment_id}")
